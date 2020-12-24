64a20b29335e7e63060971cbec3dbe99ea8f5c5c
Ezequiel <ezequiel@apptitude.com.ar>
2020-12-24 12:07:09 -0300

    update client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -31,12 +31,10 @@ class ClientController extends Controller
     public function store(ClientStoreRequest $request)
     {
         $newClient = $request->all();
-        try{
-            $client = Client::create($newClient);
-            return response()->json($client);
-        } catch(Exception $e){
-            return response()->json($e->getMessage(),500);
-        }
+        $client = Client::create($newClient);
+        
+        return response()->json($client);
+        
     }
 
     /**
@@ -64,8 +62,41 @@ class ClientController extends Controller
         if ($request->has('name') && $request->input('name') != '') {
             $client->name = $request->input('name');
         }
-
+        if ($request->has('surname') && $request->input('surname') != '') {
+            $client->surname = $request->input('surname');
+        }
+        if ($request->has('address') && $request->input('address') != '') {
+            $client->address = $request->input('address');
+        }
+        if ($request->has('floor') && $request->input('floor') != '') {
+            $client->floor = $request->input('floor');
+        }
+        if ($request->has('phone1') && $request->input('phone1') != '') {
+            $client->phone1 = $request->input('phone1');
+        }
+        if ($request->has('phone2') && $request->input('phone2') != '') {
+            $client->phone2 = $request->input('phone2');
+        }
+        if ($request->has('email') && $request->input('email') != '') {
+            $client->email = $request->input('email');
+        }
+        if ($request->has('dni') && $request->input('dni') != '') {
+            $client->dni = $request->input('dni');
+        }
+        if ($request->has('cuit') && $request->input('cuit') != '') {
+            $client->cuit = $request->input('cuit');
+        }
+        if ($request->has('city_id') && $request->input('city_id') != '') {
+            $client->city_id = $request->input('city_id');
+        }
+        if ($request->has('gender') && $request->input('gender') != '') {
+            $client->gender = $request->input('gender');
+        }
+        if ($request->has('ranking') && $request->input('ranking') != '') {
+            $client->ranking = $request->input('ranking');
+        }
         $client->save();
+
         return response()->json($client);
     }
 
@@ -79,6 +110,7 @@ class ClientController extends Controller
     {
         $client = Client::findOrFail($id);
         $client->delete();
+
         return response()->json(null,204);
 
     }
